f9add45a1ad36d805198ec75b7de9f967fa1183a
Eric <ericandrade085@gmail.com>
2021-08-18 19:30:56 -0300

    Final

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 let clickSecurity = false
+
 function getStart(duration) {
     if (!clickSecurity) {
         const start = document.querySelector('#minutes')
+        const pause = document.querySelector('#pause')
         clickSecurity = true
-        var helo = start.onclick = setInterval(function () {
+        var startPomodoro = setInterval(function () {
             if (duration >= 0) {
                 let minutes = parseInt(duration / 60)
                 let seconds = parseInt(duration % 60)
                 minutes = minutes < 10 ? '0' + minutes : minutes
                 seconds = seconds < 10 ? '0' + seconds : seconds
                 start.innerHTML = `${minutes}:${seconds}`
-                console.log(duration)
                 if (duration <= 0) {
                     start.innerHTML = 'ShortBreak'
                 }
                 --duration
+
             }
         }, 1000)
+        pause.addEventListener ('click', function () {
+            clearInterval(startPomodoro)
+        })
     }
 }
