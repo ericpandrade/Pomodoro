05c3919c44e8e679ad965b03febec7afa3102867
Eric <ericandrade085@gmail.com>
2021-08-19 18:17:06 -0300

    Finalização da Lógica

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-let clickSecurity = false
+let duration = 1500
+let isStart = false
 
-function getStart(duration) {
-    if (!clickSecurity) {
-        const start = document.querySelector('#minutes')
-        const pause = document.querySelector('#pause')
-        clickSecurity = true
-        const startPomodoro = setInterval(function () {
-            if (duration >= 0) {
-                let minutes = parseInt(duration / 60)
-                let seconds = parseInt(duration % 60)
-                minutes = minutes < 10 ? '0' + minutes : minutes
-                seconds = seconds < 10 ? '0' + seconds : seconds
-                start.innerHTML = `${minutes}:${seconds}`
-                if (duration <= 0) {
-                    start.innerHTML = 'ShortBreak'
-                }
-                --duration
-            }
+const startPomodoro = () => {
+    const time = document.querySelector('#minutes')
+    const button = document.getElementById('buttonPlay')
+    isStart = true
+
+    if (duration >= 0 && isStart) {
+        setTimeout(function () {
+            let minutes = parseInt(duration / 60)
+            let seconds = parseInt(duration % 60)
+            minutes = minutes < 10 ? '0' + minutes : minutes
+            seconds = seconds < 10 ? '0' + seconds : seconds
+            time.innerHTML = `${minutes}:${seconds}`
+            duration--
+            isStart ? button.disabled = true : button.disabled = false
+            isStart && startPomodoro()
         }, 1000)
-        pause.addEventListener ('click', function () {
-            clearInterval(startPomodoro)
-        })
+    }
+
+    if (duration < 0){
+        minutes.innerHTML = 'ShortBreak'
+        isStart = false
+        button.disabled = false
     }
 }
- 
+
+const stopPomodoro = () => {
+    isStart = false
+}
+
+const resetPomodoro = () => {
+    duration = 1500
+    isStart = false
+}
+
